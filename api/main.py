25ae0e0905d704074f3f3bf708aefcc2680bae06
diego medeiros <diego.medeiros@geowellex.com>
2022-06-08 16:13:46 -0300

    api funcionou

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+from operator import index
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
@@ -6,7 +7,8 @@ import wandb
 import pandas as pd
 import sys
 
-from source.mlops.api.preprocessing import *
+from preprocessing import *
+import train as train
 
 app = FastAPI()
 
@@ -32,25 +34,23 @@ class Heart_metrics(BaseModel):
 
     class config:
         schema_extra = {
-            "example": {
-                "BMI": 16.4,
-                "Smoking": "Yes",
-                "AlcoholDrinking": "No",
-                "Stroke": "No",
-                "PhysicalHealth": 3.0,
-                "MentalHealth": 30.0,
-                "DiffWalking": "No",
-                "Sex": "Female",
-                "AgeCategory": "55-59",
-                "Race": "White",
-                "Diabetic": "Yes",
-                "PhysicalActivity": "Yes",
-                "GenHealth": "Very good",
-                "SleepTime": 5.0,
-                "Asthma": "Yes",
-                "KidneyDisease": "No",
-                "SkinCancer": "Yes",
-            }
+            "BMI": 16.4,
+            "Smoking": "Yes",
+            "AlcoholDrinking": "No",
+            "Stroke": "No",
+            "PhysicalHealth": 3.0,
+            "MentalHealth": 30.0,
+            "DiffWalking": "No",
+            "Sex": "Female",
+            "AgeCategory": "55-59",
+            "Race": "White",
+            "Diabetic": "Yes",
+            "PhysicalActivity": "Yes",
+            "GenHealth": "Very good",
+            "SleepTime": 5.0,
+            "Asthma": "Yes",
+            "KidneyDisease": "No",
+            "SkinCancer": "Yes",
         }
 
 
@@ -68,10 +68,12 @@ async def heart_prediciton(data: Heart_metrics):
     artifact = run.use_artifact(
         "diego25rm/project_heart/model_export:v4", type="pipeline_artifact"
     ).file()
-    # try:
     loaded_model = joblib.load(artifact)
-    X = pd.DataFrame([data])
-    prediction = loaded_model.predict(X)
-    return {"Prediction": prediction}
-    # except:
-    #     return artifact
+    dict_data = dict(data)
+    data_as_list = []
+    data_as_list.append(dict_data)
+    X = pd.DataFrame([dict_data])
+    print(X)
+    prediction = {}
+    prediction["result"] = loaded_model.predict(X).tolist()[0]
+    return {"prediction": dict(prediction)}
--- api/preprocessing.py
+++ api/preprocessing.py
@@ -1,6 +1,7 @@
 import logging
 from multiprocessing import Value
 import pandas as pd
+from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -13,6 +14,16 @@ import numpy as np
 
 
 def get_numerical_categorical_features(data: pd.DataFrame):
+    """iterate into the data columns and analyse the column data type to
+    select the numeric and the string objects in different lists
+
+    Args:
+        data (pd.DataFrame): the data to analyse the columns types
+
+    Returns:
+        tuple(list,list): two lists with the numerical and categorical columns
+        respectively
+    """
     numerical_features = []
     categorical_features = []
     for i in data.columns:
@@ -24,6 +35,16 @@ def get_numerical_categorical_features(data: pd.DataFrame):
 
 
 def split_categorical_binary_features(data: pd.DataFrame):
+    """analyse the categorical data to select the size of unique values and split the columns
+    into multilabel and binary columns
+
+    Args:
+        data (pd.DataFrame): categorical data to split the categories
+
+    Returns:
+        tuple(list,list): two lists with the binary and multilabel categorical columns
+        respectively
+    """
     binary_features = []
     categorical_features = []
     for i in data.columns:
@@ -35,6 +56,16 @@ def split_categorical_binary_features(data: pd.DataFrame):
 
 
 def treat_numerical_data(data: pd.DataFrame, scaler: int):
+    """takes the data and one number to choose what is the best scaler for that data according
+    to the user, and return with a mapper to the columns names cognization
+
+    Args:
+        data (pd.DataFrame): numerical data
+        scaler (int): choosing the best scaler
+
+    Returns:
+        sklearn.preprocessing object : encoder
+    """
     if scaler == 0:
         scaler_model = StandardScaler()
     elif scaler == 1:
@@ -49,20 +80,35 @@ def treat_numerical_data(data: pd.DataFrame, scaler: int):
 
 
 def treat_categorical_data(data: pd.DataFrame, encoder):
+    """takes the data and one number to choose what is the best scaler for that data according
+    to the user, and return the categorical encoder with.
+
+    Args:
+        data (pd.DataFrame): categorical data
+        scaler (int): choosing the best scaler
+
+    Returns:
+        sklearn.preprocessing object : encoder
+    """
     if encoder == 0:
-        # labeled_features_df = pd.get_dummies(data=data, prefix=data.columns)
         treat_model = OneHotEncoder(sparse=False, drop="first")
     else:
         treat_model = LabelEncoder()
-        # data2 = data.copy()
-        # for i in data.columns:
-        #     data2[i] = le.fit(data2[i])
-        # labeled_features_df = data2
 
     return treat_model
 
 
 def resample_data_stratified(data: pd.DataFrame, column: str):
+    """get the dataset and applies a downsample operation to equals the number of values
+    by categorie according to the column passed as argument
+
+    Args:
+        data (pd.DataFrame): full dataset
+        column (str): column that guides the downsample
+
+    Returns:
+        pd.DataFrame: the data reduced by resample
+    """
     min_sample = min(data[column].value_counts())
     resample_data = (
         data.groupby(column, group_keys=False)
@@ -73,6 +119,15 @@ def resample_data_stratified(data: pd.DataFrame, column: str):
 
 
 def inputation_categorical_data(data: pd.DataFrame, inputation_type: int):
+    """choice of the best inputation for the categorical data according to the user
+
+    Args:
+        data (pd.DataFrame): full dataset
+        inputation_type (int): number of the inputation in the if and else
+
+    Returns:
+        pbject : inputer object
+    """
     categorical_columns = data.columns.to_list()
     if inputation_type == 0:
         imp = SimpleImputer(strategy="most_frequent")
@@ -82,6 +137,15 @@ def inputation_categorical_data(data: pd.DataFrame, inputation_type: int):
 
 
 def inputation_numerical_data(data: pd.DataFrame, inputation_type: int):
+    """choice of the best inputation for the numerical data according to the user
+
+    Args:
+        data (pd.DataFrame): full dataset
+        inputation_type (int): number of the inputation in the if and else
+
+    Returns:
+        pbject : inputer object
+    """
     if inputation_type == 0:
         imp = SimpleImputer(strategy="mean")
     elif inputation_type == 1:
@@ -95,7 +159,17 @@ def inputation_numerical_data(data: pd.DataFrame, inputation_type: int):
 
 
 def remove_outliers(numerical_data: pd.DataFrame, column: str):
+    """Find the first and third quartil and remove the outliers of the numerical data according
+    to the range of values produced by : lim_inferior = quartil1 - 1.5 * rangeinterquartil
+    lim_superior = quartil3 - 1.5 * rangeinterquartil
 
+    Args:
+        numerical_data (pd.DataFrame): numerical dataset
+        column (str): column to remove the outliers
+
+    Returns:
+        pd.DataFrame: dataset without outliers in the column passed as argument
+    """
     q25, q75 = np.percentile(numerical_data[column], 25), np.percentile(
         numerical_data[column], 75
     )
@@ -111,10 +185,19 @@ def remove_outliers(numerical_data: pd.DataFrame, column: str):
 
 
 class Feature_selector(BaseEstimator, TransformerMixin):
+    """class to select and split the dataset between categorical and numerical
+
+    Args:
+        BaseEstimator (_type_): extends BaseEstimator
+        TransformerMixin (_type_): extends TransformerMixin
+    """
+
     def __init__(self, data_type="numerical"):
+        """Feature_selector class builder"""
         self.data_type = data_type
 
     def fit(self, X: pd.DataFrame):
+        """select the numerical and categorical columns"""
         (
             self.numerical_features,
             self.categorical_features,
@@ -122,6 +205,14 @@ class Feature_selector(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X: pd.DataFrame):
+        """split the dataset into numerical and categorical data
+
+        Args:
+            X (pd.DataFrame): dataset
+
+        Returns:
+            pd.DataFrame: dataset splitted to be numerical or categorical
+        """
 
         if self.data_type == "numerical":
             return X.loc[:, self.numerical_features]
@@ -130,6 +221,10 @@ class Feature_selector(BaseEstimator, TransformerMixin):
 
 
 class Preprocessing_initial_data(BaseEstimator, TransformerMixin):
+    """class to resample and remove the outliers, separated function to change
+    the indexs of the full dataset
+    """
+
     def __init__(
         self,
         remove_outlier=False,
@@ -137,15 +232,32 @@ class Preprocessing_initial_data(BaseEstimator, TransformerMixin):
         resample=True,
         target_column=None,
     ):
+        """Preprocessing_initial_data class builder, the user select the parameters to initial preprocessing
+
+        Args:
+            remove_outlier (bool, optional): if remove outliers or not. Defaults to False.
+            columns_with_outliers (list, optional): columsn to remove the outliers. Defaults to [].
+            resample (bool, optional): if you want to resample the dataset. Defaults to True.
+            target_column (_type_, optional): column to base downsample. Defaults to None.
+        """
         self.remove_outlier = remove_outlier
         self.columns_with_outliers = columns_with_outliers
         self.resample = resample
         self.target_column = target_column
 
     def fit(self, X):
+        """nothing yet"""
         return self
 
     def transform(self, X):
+        """resample and remove the data outliers depending to the user choices
+
+        Args:
+            X (pd.DataFrame): dataset
+
+        Returns:
+            _type_: dataset with dowsample, remove outliers or resample
+        """
         if self.resample:
             X = resample_data_stratified(X, self.target_column)
         if self.remove_outlier:
@@ -155,6 +267,14 @@ class Preprocessing_initial_data(BaseEstimator, TransformerMixin):
 
 
 class Categorical_tranformer(BaseEstimator, TransformerMixin):
+    """class to tranform the categorical data, applying the inputation and the encoders
+    according to the user choice
+
+    Args:
+        BaseEstimator (_type_): extends BaseEstimator
+        TransformerMixin (_type_): extends TransformerMixin
+    """
+
     def __init__(
         self,
         inputation_categorical=True,
@@ -163,6 +283,15 @@ class Categorical_tranformer(BaseEstimator, TransformerMixin):
         treat_type=0,
         colnames=[],
     ):
+        """Categorical_tranformer class builder
+
+        Args:
+            inputation_categorical (bool, optional): if gonna applies an inputation. Defaults to True.
+            inputation_type (int, optional): what is the inputation?. Defaults to 0.
+            treat_categorical (bool, optional): if gonna applies an encoder. Defaults to False.
+            treat_type (int, optional): what is the encoder?. Defaults to 0.
+            colnames (list, optional): categorical columns to reference. Defaults to [].
+        """
         self.inputation_categorical = inputation_categorical
         self.inputation_type = inputation_type
         self.treat_categorical = treat_categorical
@@ -170,10 +299,19 @@ class Categorical_tranformer(BaseEstimator, TransformerMixin):
         self.colnames = colnames
 
     def get_feature_names_out(self):
+        """returns the categorical columns passed to the class"""
         return self.colnames
 
     def fit(self, X, y=None):
-        # X = Feature_selector(data_type="categorical").fit_transform(X)
+        """fit the inputation and/or the encoder according to the user choice
+
+        Args:
+            X (pd.DataFrame): raw data
+            y (pd.DataFrame, optional): target column in the dataset. Defaults to None.
+
+        Returns:
+            Categorical_tranformer: object with preprocessing model trained
+        """
         if self.inputation_categorical:
             self.categorical_inputation = inputation_categorical_data(
                 X, self.inputation_type
@@ -190,7 +328,15 @@ class Categorical_tranformer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
-        # X = Feature_selector(data_type="categorical").fit_transform(X)
+        """applies the transformation in the categorical data
+
+        Args:
+            X (pd.DataFrame): raw dataset
+            y (pd.DataFrame, optional): target column. Defaults to None.
+
+        Returns:
+            pd.DataFrame: transformed data
+        """
         if self.inputation_categorical:
             for i in X.columns:
                 X[i] = self.categorical_inputation.fit_transform(
@@ -204,6 +350,13 @@ class Categorical_tranformer(BaseEstimator, TransformerMixin):
 
 
 class Numerical_tranformer(BaseEstimator, TransformerMixin):
+    """class to transform the numerical data
+
+    Args:
+        BaseEstimator (_type_): extends BaseEstimator
+        TransformerMixin (_type_): extends TransformerMixin
+    """
+
     def __init__(
         self,
         inputation_numerical=False,
@@ -212,6 +365,15 @@ class Numerical_tranformer(BaseEstimator, TransformerMixin):
         treat_type=0,
         colnames=[],
     ):
+        """Numerical_tranformer class builder
+
+        Args:
+            inputation_numerical (bool, optional): if gonna applies an inputation. Defaults to True.
+            inputation_type (int, optional): what is the inputation?. Defaults to 0.
+            treat_numerical (bool, optional): if gonna applies an encoder. Defaults to False.
+            treat_type (int, optional): what is the encoder?. Defaults to 0.
+            colnames (list, optional): categorical columns to reference. Defaults to [].
+        """
         self.inputation_numerical = inputation_numerical
         self.inputation_type = inputation_type
         self.treat_numerical = treat_numerical
@@ -219,10 +381,19 @@ class Numerical_tranformer(BaseEstimator, TransformerMixin):
         self.colnames = colnames
 
     def get_feature_names_out(self):
+        """return the numerical dataset's columns"""
         return self.colnames
 
     def fit(self, X, y=None):
-        # X = Feature_selector(data_type="numerical").fit_transform(X)
+        """fit the inputation and/or the encoder according to the user choice
+
+        Args:
+            X (pd.DataFrame): raw data
+            y (pd.DataFrame, optional): target column in the dataset. Defaults to None.
+
+        Returns:
+            Numerical_tranformer: object with preprocessing model trained
+        """
         if self.inputation_numerical:
             self.input_numerical = inputation_numerical_data(X, self.inputation_type)
 
@@ -233,6 +404,15 @@ class Numerical_tranformer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
+        """applies the transformation in the numerical data
+
+        Args:
+            X (pd.DataFrame): raw dataset
+            y (pd.DataFrame, optional): target column. Defaults to None.
+
+        Returns:
+            pd.DataFrame: transformed data
+        """
         X = Feature_selector(data_type="numerical").fit_transform(X)
         if self.treat_numerical:
             scaled_features = self.treat_numerical.transform(X.copy())
@@ -246,6 +426,14 @@ class Numerical_tranformer(BaseEstimator, TransformerMixin):
 
 
 class pipeline_preprocessing(BaseEstimator, TransformerMixin):
+    """union of the numerical transformer and the categorical transformer to create a pipeline object
+    to process the data
+
+    Args:
+        BaseEstimator (_type_): extends BaseEstimator
+        TransformerMixin (_type_): extends TransformerMixin
+    """
+
     def __init__(
         self,
         inputation_numerical=False,
@@ -257,6 +445,20 @@ class pipeline_preprocessing(BaseEstimator, TransformerMixin):
         treat_categorical=True,
         treat_cat_type=1,
     ):
+        """pipeline_preprocessing class builder
+
+        Args:
+            inputation_numerical (bool, optional): if it gonna applies the inputation into numerical dataset
+            .Defaults to False.
+            inputation_type (int, optional): type of the inputation to numeric dataset. Defaults to 0.
+            treat_numerical (bool, optional): if gonna encode numerical dataset. Defaults to True.
+            treat_type (int, optional): whats is the numerical encoder. Defaults to 1.
+            inputation_categorical (bool, optional): if it gonna applies the inputation into categorical
+            dataset. Defaults to True.
+            inputation_cat_type (int, optional):type of the inputation to categorical dataset. Defaults to 0.
+            treat_categorical (bool, optional): if gonna encode categorical dataset. Defaults to True.
+            treat_cat_type (int, optional): whats is the categorical encoder. Defaults to 1.
+        """
         self.inputation_categorical = inputation_categorical
         self.inputation_type = inputation_type
         self.treat_categorical = treat_categorical
@@ -267,6 +469,16 @@ class pipeline_preprocessing(BaseEstimator, TransformerMixin):
         self.treat_cat_type = treat_cat_type
 
     def fit(self, X: pd.DataFrame, y=None):
+        """build categorical and numerical pipelines and applies an union between the
+        2 pipelines
+
+        Args:
+            X (pd.DataFrame): raw dataset
+            y (_type_, optional): target column. Defaults to None.
+
+        Returns:
+             preprocessing_pipeline : pipeline model trained
+        """
         numerical_features, categorical_features = get_numerical_categorical_features(X)
 
         transform_numerical = Numerical_tranformer(
@@ -301,11 +513,19 @@ class pipeline_preprocessing(BaseEstimator, TransformerMixin):
             ]
         )
         self.categorical_pipeline.fit(X)
-        # if len(y) > 0:
-        #     self.answer = LabelEncoder().fit(y.reshape(-1, 1))
+
         return self
 
     def transform(self, X: pd.DataFrame, y=None):
+        """transform the raw data
+
+        Args:
+            X (pd.DataFrame): raw dataset
+            y (_type_, optional): target column. Defaults to None.
+
+        Returns:
+           pd.DataFrame: trsnaformed data
+        """
         pipeline_preprocessing = FeatureUnion(
             transformer_list=[
                 ("numerical_pipeline", self.numerical_pipeline),
@@ -325,9 +545,5 @@ class pipeline_preprocessing(BaseEstimator, TransformerMixin):
         )
         colnames = categorical_cols + numerical_cols
         X = pd.DataFrame(pipeline_preprocessing.transform(X), columns=colnames)
-        # if len(y) > 0:
-        #     y = pd.DataFrame(
-        #         self.answer().transform(y.reshape(-1, 1)),
-        #         columns=["HeartDisease"],
-        #     )
+
         return X
